71352491f832dadefc65fb0539c52ac87368f089
SaraMart <sara.martinez965@unir.net>
2026-01-10 18:02:14 +0100

    Creación de componentes Header y Footer. Crear Rutas para menu de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
+import { Recetas } from './pages/recetas/recetas';
+import { Productos } from './pages/productos/productos';
+import { Carrito } from './pages/carrito/carrito';
+import { Usuario } from './pages/usuario/usuario';
 
-export const routes: Routes = [];
+//Aquí van las rutas en el proyecto!!
+export const routes: Routes = [
+
+    {path:'', component: Home}, //Ruta para la página de inicio
+    {path:'Sobre nosotros', component: SobreNosotros}, //Ruta para la página de Sobre nosotros
+    {path:'Recetas', component: Recetas}, //Ruta para la página de recetas
+    {path:'Productos', component: Productos}, //Ruta para la página de productos
+    {path:'Carrito', component: Carrito}, //Ruta para la página de carrito
+    {path:'Usuario', component: Usuario}, //Ruta para la página de usuario
+    {path:'**', redirectTo:''}, //Ruta error = redireccionamos a la ruta RAIZ
+
+
+];
