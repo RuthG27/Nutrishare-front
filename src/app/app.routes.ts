134e58e7825bc957cbbc4c502cb67b2f036cee8a
SaraMart <sara.martinez965@unir.net>
2026-01-11 01:08:41 +0100

    Primera carga de productos realizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
 import { Recetas } from './pages/recetas/recetas';
-import { Productos } from './pages/productos/productos';
+import { ProductosSer } from './pages/productos/productos';
 import { Carrito } from './pages/carrito/carrito';
 import { Usuario } from './pages/usuario/usuario';
 
@@ -12,7 +12,7 @@ export const routes: Routes = [
     {path:'', component: Home}, //Ruta para la página de inicio
     {path:'Sobre nosotros', component: SobreNosotros}, //Ruta para la página de Sobre nosotros
     {path:'Recetas', component: Recetas}, //Ruta para la página de recetas
-    {path:'Productos', component: Productos}, //Ruta para la página de productos
+    {path:'Productos', component: ProductosSer}, //Ruta para la página de productos
     {path:'Carrito', component: Carrito}, //Ruta para la página de carrito
     {path:'Usuario', component: Usuario}, //Ruta para la página de usuario
     {path:'**', redirectTo:''}, //Ruta error = redireccionamos a la ruta RAIZ
--- src/app/services/productos.ts
+++ src/app/services/productos.ts
@@ -16,9 +16,10 @@ export interface Producto{
 
 
 export class Productos {
-  private productos:Producto [] = [
+  private productos:Producto [] = 
 
-     {
+[
+  {
     "id": "64f1a0000000000000000001",
     "nombre": "Aceite de oliva virgen",
     "categoria": "Aceites y grasas",
@@ -47,7 +48,99 @@ export class Productos {
     "nombre": "Garbanzos cocidos",
     "categoria": "Legumbres",
     "img": "https://imagenes.ruthunir.com/ingredientes/garbanzos-cocidos.png"
+  },
+  {
+    "id": "64f1a0000000000000000006",
+    "nombre": "Ajo",
+    "categoria": "Hortalizas",
+    "img": "https://imagenes.ruthunir.com/ingredientes/ajo.png"
+  },
+  {
+    "id": "64f1a0000000000000000007",
+    "nombre": "Yogur natural",
+    "categoria": "Lácteos",
+    "img": "https://imagenes.ruthunir.com/ingredientes/yogur-natural.png"
+  },
+  {
+    "id": "64f1a0000000000000000008",
+    "nombre": "Tomate",
+    "categoria": "Hortalizas",
+    "img": "https://imagenes.ruthunir.com/ingredientes/tomate.png"
+  },
+  {
+    "id": "64f1a0000000000000000009",
+    "nombre": "Queso feta",
+    "categoria": "Quesos",
+    "img": "https://imagenes.ruthunir.com/ingredientes/queso-feta.png"
+  },
+  {
+    "id": "64f1a000000000000000000a",
+    "nombre": "Pan de pita",
+    "categoria": "Pan y cereales",
+    "img": "https://imagenes.ruthunir.com/ingredientes/pan-de-pita.png"
+  },
+  {
+    "id": "64f1a000000000000000000b",
+    "nombre": "Queso mozzarella",
+    "categoria": "Quesos",
+    "img": "https://imagenes.ruthunir.com/ingredientes/queso-mozzarella.png"
+  },
+  {
+    "id": "64f1a000000000000000000c",
+    "nombre": "Queso parmesano",
+    "categoria": "Quesos",
+    "img": "https://imagenes.ruthunir.com/ingredientes/queso-parmesano.png"
+  },
+  {
+    "id": "64f1a000000000000000000d",
+    "nombre": "Albahaca",
+    "categoria": "Hierbas y especias",
+    "img": "https://imagenes.ruthunir.com/ingredientes/albahaca.png"
+  },
+  {
+    "id": "64f1a000000000000000000e",
+    "nombre": "Orégano",
+    "categoria": "Hierbas y especias",
+    "img": "https://imagenes.ruthunir.com/ingredientes/oregano.png"
+  },
+  {
+    "id": "64f1a000000000000000000f",
+    "nombre": "Pan",
+    "categoria": "Pan y cereales",
+    "img": "https://imagenes.ruthunir.com/ingredientes/pan.png"
+  },
+  {
+    "id": "64f1a0000000000000000010",
+    "nombre": "Pasta (cocida)",
+    "categoria": "Pan y cereales",
+    "img": "https://imagenes.ruthunir.com/ingredientes/pasta.png"
+  },
+  {
+    "id": "64f1a0000000000000000011",
+    "nombre": "Vinagre balsámico",
+    "categoria": "Salsas y vinagres",
+    "img": "https://imagenes.ruthunir.com/ingredientes/vinagre-balsamico.png"
+  },
+  {
+    "id": "64f1a0000000000000000012",
+    "nombre": "Aceitunas negras",
+    "categoria": "Conservas y encurtidos",
+    "img": "https://imagenes.ruthunir.com/ingredientes/aceitunas-negras.png"
+  },
+  {
+    "id": "64f1a0000000000000000013",
+    "nombre": "Cebolla roja",
+    "categoria": "Hortalizas",
+    "img": "https://imagenes.ruthunir.com/ingredientes/cebolla-roja.png"
+  },
+  {
+    "id": "64f1a0000000000000000014",
+    "nombre": "Pimiento rojo",
+    "categoria": "Hortalizas",
+    "img": "https://imagenes.ruthunir.com/ingredientes/pimiento-rojo.png"
   }
+
+
 
 ]
 
