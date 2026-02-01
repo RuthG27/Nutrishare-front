ec17d04daa06acc45bd62f49e803d58ce7ad1471
imartinezig <irenemartineziglesias@gmail.com>
2026-02-01 21:31:58 +0100

    Página de detalle de las recetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home';
 import { SobreNosotrosComponent } from './modules/about/sobre-nosotros';
 import { RecetasComponent } from './modules/recetas/recetas';
+import { DetalleComponent } from './modules/recetas/detalle/detalle';
 import { ProductosComponent } from './modules/productos/productos';
 import { CarritoComponent } from './modules/carrito/carrito';
 import { UsuarioComponent } from './modules/profile/usuario';
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: '', component: HomeComponent }, //Ruta para la página de inicio
   { path: 'sobre-nosotros', component: SobreNosotrosComponent }, //Ruta para la página de Sobre nosotros
   { path: 'recetas', component: RecetasComponent }, //Ruta para la página de recetas
+  { path: 'recetas/:id', component: DetalleComponent }, //Ruta para las página de detalle de las recetas
   { path: 'productos', component: ProductosComponent }, //Ruta para la página de productos
   { path: 'carrito', component: CarritoComponent }, //Ruta para la página de carrito
   { path: 'usuario', component: UsuarioComponent }, //Ruta para la página de usuario
--- src/app/modules/recetas/recetas.ts
+++ src/app/modules/recetas/recetas.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Receta } from '../../services/recetas';
 import { Recetas } from '../../services/recetas';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-recetas',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recetas.html',
   styleUrl: './recetas.css',
 })
